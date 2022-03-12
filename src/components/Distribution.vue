aeca69709b6cc12890df04c7852b7134a334dfa8
Dan <rusnac14@gmail.com>
2022-03-12 18:29:22 +0100

    Group projects by chain

--- src/components/Distribution.vue
+++ src/components/Distribution.vue
@@ -4,8 +4,11 @@
       <v-row>
         <h5>AIRDROPPED PROJECTS (SO FAR...)</h5>
       </v-row>
-      <v-row class="mt-15">
-        <v-avatar v-for="(project, index) in airdroppedProjects" :key="index" style="" class="pointerOnHover ma-1">
+      <v-row class="mt-10" >
+        <h6>ETHEREUM</h6>
+      </v-row>
+      <v-row class="mt-4">
+        <v-avatar v-for="(project, index) in projects('eth')" :key="index" size="35" class="pointerOnHover ma-1">
           <v-tooltip bottom>
             <template v-slot:activator="{ on, attrs }">
                 <v-img :src="project.logo" @click="goTo(project.website)" v-bind="attrs" v-on="on"/>
@@ -15,33 +18,86 @@
 
         </v-avatar>
       </v-row>
+
+      <v-row class="mt-6" >
+        <h6>GNOSIS CHAIN</h6>
+      </v-row>
+      <v-row class="mt-4">
+        <v-avatar v-for="(project, index) in projects('xdai')" :key="index" size="40" class="pointerOnHover ma-1">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img :src="project.logo" @click="goTo(project.website)" v-bind="attrs" v-on="on"/>
+            </template>
+            <span>{{project.name}}</span>
+          </v-tooltip>
+        </v-avatar>
+      </v-row>
+
+      <v-row class="mt-6" >
+        <h6>AVALANCHE</h6>
+      </v-row>
+      <v-row class="mt-4">
+        <v-avatar v-for="(project, index) in projects('avax')" :key="index" size="40" class="pointerOnHover ma-1">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img :src="project.logo" @click="goTo(project.website)" v-bind="attrs" v-on="on"/>
+            </template>
+            <span>{{project.name}}</span>
+          </v-tooltip>
+        </v-avatar>
+      </v-row>
+
+      <v-row class="mt-6" >
+        <h6>POLYGON</h6>
+      </v-row>
+      <v-row class="mt-4">
+        <v-avatar v-for="(project, index) in projects('polygon')" :key="index" size="40" class="pointerOnHover ma-1">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img :src="project.logo" @click="goTo(project.website)" v-bind="attrs" v-on="on"/>
+            </template>
+            <span>{{project.name}}</span>
+          </v-tooltip>
+        </v-avatar>
+      </v-row>
+
+      <v-row class="mt-6" >
+        <h6>BINANCE CHAIN</h6>
+      </v-row>
+      <v-row class="mt-4">
+        <v-avatar v-for="(project, index) in projects('bnb')" :key="index" size="40" class="pointerOnHover ma-1">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-img :src="project.logo" @click="goTo(project.website)" v-bind="attrs" v-on="on"/>
+            </template>
+            <span>{{project.name}}</span>
+          </v-tooltip>
+        </v-avatar>
+      </v-row>
     </v-col>
     <v-col colls-md="6"  colls-md-offset="1" colls-xs="12" colls-sm="12" >
       <v-row>
-        <h2>BIGGEST CROSS-CHAIN AIRDROP IN NFT HISTORY!</h2>
+        <h2>A MULTICHAIN NFT COLLECTION</h2>
       </v-row>
       <v-row class="mt-10" style="text-align: justify;">
         <p>
           We think art shouldn't have barries, so it's crazy to create artificial ones. This is why at SNAFU we decided to provide nft bridges to our users with the help of XP.Network.
         </p>
-      </v-row>
-
-      <v-row class="mt-10" >
-        <h4>MULTICHAIN COLLECTION</h4>
-      </v-row>
-      <v-row>
         <p class="mt-5" style="text-align: justify;">
           To celebrate this achievement we decided to mint our unique Bored David NFTs on <b>Ethereum, Gnosis, Avalanche, Polygon and Binance Smart Chain</b>.
         </p>
       </v-row>
 
       <v-row class="mt-10" >
-        <h4>AIRDROP</h4>
+        <h4>THE BIGGEST MULTICHAIN AIRDROP IN NFT HISTORY!</h4>
       </v-row>
       <v-row>
         <p class="mt-5" style="text-align: justify;">
           We are airdropping the majority of NFTs to projects, dev teams and communities that provide usefully resources, support and applications to the web3 community.
           Right now we are whitelisting all the addresses, when we will go live you will be able to mint the NFTs for free.
+        </p>
+        <p>
+          Want to receive the airdrop? <a href="https://discord.gg/kvbxMfUyQD" target="_blank">Get in touch with us</a>.
         </p>
       </v-row>
     </v-col>
@@ -49,6 +105,7 @@
 </template>
 
 <script>
+import {filter} from 'lodash'
 export default {
   components: {
 
@@ -56,6 +113,9 @@ export default {
   methods: {
     goTo(url) {
       window.open(url, '_blank')
+    },
+    projects(blockchainCode) {
+      return filter(this.airdroppedProjects, {blockchain: blockchainCode})
     }
   },
   data() {
@@ -64,42 +124,68 @@ export default {
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'eth'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'eth'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'xdai'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'xdai'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'xdai'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'xdai'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'xdai'
         },
         {
           name: 'NFBeez',
           website: '',
-          logo: '/projects/twitter.png'
+          logo: '/projects/twitter.png',
+          blockchain: 'avax'
+        },
+        {
+          name: 'NFBeez',
+          website: '',
+          logo: '/projects/twitter.png',
+          blockchain: 'avax'
+        },
+        {
+          name: 'NFBeez',
+          website: '',
+          logo: '/projects/twitter.png',
+          blockchain: 'bnb'
+        },
+        {
+          name: 'NFBeez',
+          website: '',
+          logo: '/projects/twitter.png',
+          blockchain: 'polygon'
         },
       ]
     }
